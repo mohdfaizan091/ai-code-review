4bdd52e09adba908afe5d2133b9aae25f11f9a0f
MOHD FAIZAN <mohdfaiz0921078@gmail.com>
2026-06-15 17:49:11 +0530

    change : update the cors policy

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -47,8 +47,8 @@ export const login = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
 
@@ -69,8 +69,9 @@ export const getMe = (req, res) => {
 export const logout = (req, res) => {
     res.clearCookie("token", {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        secure:true ,
+        sameSite: "none",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     res.status(200).json({ message: "Logged out successfully" });
 };
